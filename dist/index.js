4ce5138cd2861b53bc7d9ce6755c853cf9c3d2cc
TuanVA <tuanva@creativeforce.io>
2022-09-09 16:53:47 +0700

    update package

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,5 @@ export var powerBy = function (num, pow) {
     if (typeof (num) !== 'number' || typeof (pow) !== 'number') {
         return Math.pow(num, pow);
     }
-    // throw new Error("Invalid type");
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export const powerBy = (num: number, pow: number) => {
   if (typeof(num) !== 'number' || typeof(pow) !== 'number') {
     return Math.pow(num, pow)
   }
-  // throw new Error("Invalid type");
 }
