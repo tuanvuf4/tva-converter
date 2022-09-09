c37839e9a0455ee03e30fc949f8b4ec3b865825b
TuanVA <tuanva@creativeforce.io>
2022-09-09 16:57:04 +0700

    update package

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
 export var powerBy = function (num, pow) {
-    if (typeof (num) !== 'number' || typeof (pow) !== 'number') {
+    if (typeof (num) === 'number' && typeof (pow) === 'number') {
         return Math.pow(num, pow);
     }
+    throw new Error("Invalid input data")
 };
 //# sourceMappingURL=index.js.map
