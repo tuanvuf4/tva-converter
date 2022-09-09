2a96a53a45b7d58cb29008134a90116e42a067ee
TuanVA <tuanva@creativeforce.io>
2022-09-09 16:59:04 +0700

    update package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export const powerBy = (num: number, pow: number) => {
-  if (typeof(num) !== 'number' || typeof(pow) !== 'number') {
+  if (typeof(num) === 'number' && typeof(pow) === 'number') {
     return Math.pow(num, pow)
   }
+  throw new Error("Invalid input data!");
+  
 }
