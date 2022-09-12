e70f9fa031a0c6647c5a8d21b4fa731cddd084e4
TuanVA <tuanva@creativeforce.io>
2022-09-12 16:46:01 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,11 @@ export var powerBy = function (num, pow) {
     }
     throw new Error("Invalid input data!");
 };
+var TVAConverter;
+(function (TVAConverter) {
+    function create() {
+        console.log('on Create ');
+    }
+    TVAConverter.create = create;
+})(TVAConverter || (TVAConverter = {}));
 //# sourceMappingURL=index.js.map
