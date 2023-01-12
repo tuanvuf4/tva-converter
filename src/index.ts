81bbb3af408d8fce9b2f1895cc6110510a0df492
Tuan Vu <tuanvuf4@gmail.com>
2023-01-12 09:03:35 +0700

    add rxjs

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ export * from './app/date';
 
 import {
   concatMapOperater,
-  foo,
-  mapOperater,
-  mergeMapOperater,
-  switchMapOperater,
+  foo, mapOperater, mergeMapOperater, switchMapOperater
 } from './app/rxjs/operaters';
 
 import { Person } from './app/decorators';
@@ -17,7 +14,6 @@ import {
   AsyncSubjectQueues,
   ReplaySubjectQueues,
 } from './app/rxjs/subjects';
-import { sourceNumber$ } from './app/rxjs';
 
 keyupListener(document.getElementById('search')).subscribe((resposne) => {
   console.log('resposne ', resposne);
@@ -32,77 +28,81 @@ keyupListener(document.getElementById('SubjectQueues')).subscribe(
 );
 
 // SubjectQueues
-SubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('SubjectQueues a ', response);
-});
-SubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('SubjectQueues b ', response);
-});
-document.getElementById('SubjectQueues').addEventListener('click', () => {
-  SubjectQueues.next(i++);
-});
+// SubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('SubjectQueues a ', response);
+// });
+// SubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('SubjectQueues b ', response);
+// });
+// document.getElementById('SubjectQueues').addEventListener('click', () => {
+//   SubjectQueues.next(i++);
+// });
 
 // BehaviorSubjectQueues
-BehaviorSubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('BehaviorSubjectQueues a ', response);
-  console.log('BehaviorSubjectQueues b ', response);
-});
-BehaviorSubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('BehaviorSubjectQueues a ', response);
-  console.log('BehaviorSubjectQueues b ', response);
-});
-document
-  .getElementById('BehaviorSubjectQueues')
-  .addEventListener('click', () => {
-    BehaviorSubjectQueues.next(i++);
-  });
+// BehaviorSubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('BehaviorSubjectQueues a ', response);
+//   console.log('BehaviorSubjectQueues b ', response);
+// });
+// BehaviorSubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('BehaviorSubjectQueues a ', response);
+//   console.log('BehaviorSubjectQueues b ', response);
+// });
+// document
+//   .getElementById('BehaviorSubjectQueues')
+//   .addEventListener('click', () => {
+//     BehaviorSubjectQueues.next(i++);
+//   });
 
 // AsyncSubjectQueues
-AsyncSubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('AsyncSubjectQueues a ', response);
-  console.log('AsyncSubjectQueues b ', response);
-});
-AsyncSubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('AsyncSubjectQueues a ', response);
-  console.log('AsyncSubjectQueues b ', response);
-});
-document.getElementById('AsyncSubjectQueues').addEventListener('click', () => {
-  AsyncSubjectQueues.next(i++);
-  AsyncSubjectQueues.complete();
-});
+// AsyncSubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('AsyncSubjectQueues a ', response);
+//   console.log('AsyncSubjectQueues b ', response);
+// });
+// AsyncSubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('AsyncSubjectQueues a ', response);
+//   console.log('AsyncSubjectQueues b ', response);
+// });
+// document.getElementById('AsyncSubjectQueues').addEventListener('click', () => {
+//   AsyncSubjectQueues.next(i++);
+//   AsyncSubjectQueues.complete();
+// });
 
 // ReplaySubjectQueues
-ReplaySubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('ReplaySubjectQueues a ', response);
-});
-ReplaySubjectQueues.subscribe((response) => {
-  console.log('=================================');
-  console.log('ReplaySubjectQueues b ', response);
-});
-document.getElementById('ReplaySubjectQueues').addEventListener('click', () => {
-  ReplaySubjectQueues.next(i++);
-  ReplaySubjectQueues.next(i + 2);
-  ReplaySubjectQueues.next(i + 3);
-  ReplaySubjectQueues.next(i + 4);
-  ReplaySubjectQueues.next(i + 4);
-});
+// ReplaySubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('ReplaySubjectQueues a ', response);
+// });
+// ReplaySubjectQueues.subscribe((response) => {
+//   console.log('=================================');
+//   console.log('ReplaySubjectQueues b ', response);
+// });
+// document.getElementById('ReplaySubjectQueues').addEventListener('click', () => {
+//   ReplaySubjectQueues.next(i++);
+//   ReplaySubjectQueues.next(i + 2);
+//   ReplaySubjectQueues.next(i + 3);
+//   ReplaySubjectQueues.next(i + 4);
+//   ReplaySubjectQueues.next(i + 4);
+// });
 
-const p1 = new Person();
-console.log('p1 ', p1);
-p1.sayName();
+// decorators
+// const p1 = new Person();
+// console.log('p1 ', p1);
+// p1.sayName();
 
 // console.log('-------------- map --------------');
 // mapOperater.subscribe((data) => console.log(data));
-
+// 
 // console.log('-------------- switchMap --------------');
 // switchMapOperater.subscribe((data) => console.log(data));
+
+// console.log('-------------- switchMap --------------');
+// foo.subscribe((data) => console.log(data));
 // 
 // console.log('-------------- mergeMap --------------');
 // mergeMapOperater.subscribe((data) => console.log(data));
@@ -110,5 +110,3 @@ p1.sayName();
 // console.log('-------------- concatMap --------------');
 // concatMapOperater.subscribe((data) => console.log(data));
 
-console.log('-------------- concatMap --------------');
-foo.subscribe((data) => console.log(data));
